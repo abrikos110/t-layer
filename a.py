a0f94322c9cee0479c40240bee22f1995f24ed2d
abrikos <abrikos1100@gmail.com>
2022-09-22 22:59:57 +0300

    Finally, animation

--- a.py
+++ a.py
@@ -1,10 +1,12 @@
 import numpy
 import matplotlib.pyplot as plt
+import time
 
 '''
 y'_t = y''_xx + f(x, t)
-y'_x (0, t) = y'_x (1, t) = 0
-y(x, 0) = ...
+y'_x (0, t) = 1
+y'_x (1, t) = 0
+y(x, 0) = [x < 0.5]
 '''
 
 def step(x, y, t, dx, dt, f):
@@ -15,7 +17,9 @@ def step(x, y, t, dx, dt, f):
     d2[0] = d2[-1] = 0
 
     ans += dt * d2 + f(x, t)  # FIRST ORDER TIME STEP
-    ans[0], ans[-1] = ans[1], ans[-2]  # FIRST ORDER BOUNDARY
+    #ans[0], ans[-1] = 1, 0
+    ans[0] = ans[1]
+    ans[-1] = ans[-2]
 
     return ans, d2
 
@@ -26,17 +30,23 @@ n, nt = 10**2, 10**9
 x = numpy.linspace(0, 1, n)
 y = (x < 0.5) * 1.0
 
-t = 1000
-dx, dt = 1/n, t/nt
+dx = 1/n
+dt = 0.5 * dx * dx
 
 lst = y
+t = 0
+
+plt.ion()
+figure, ax = plt.subplots(figsize=(10, 8))
+line1, = ax.plot(x, y)
+figure.canvas.draw()
+figure.canvas.flush_events()
 for i in range(nt):
     a, d = step(x, lst, i*dt, dx, dt, zero)
     lst = a
-#print(ans[::10])
-    if i % 100000 == 0:
-        print(dt)
-        plt.plot(x, lst)
-        plt.show()
-    dt = 0.001 / abs(d).max()
-    dt = min(0.5 * dx*dx, dt)
+    if time.time() - t > 0.1:
+        print(dt, i, i*dt)
+        line1.set_ydata(lst)
+        figure.canvas.draw()
+        figure.canvas.flush_events()
+        t = time.time()
