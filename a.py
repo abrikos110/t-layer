8921eae410624919be85d019b965790b3932cdf5
abrikos <abrikos1100@gmail.com>
2022-09-25 18:04:01 +0300

    Fixed bug with lack of dx^2 in second derivative

--- a.py
+++ a.py
@@ -14,9 +14,10 @@ def step(x, y, t, dx, dt, f):
 
     ans = y.copy()
     d2 = R(y, -1) - 2*y + R(y, 1)
+    d2 /= dx*dx
     d2[0] = d2[-1] = 0
 
-    ans += dt * d2 + f(x, t)  # FIRST ORDER TIME STEP
+    ans += dt * (d2 + f(x, t))  # FIRST ORDER TIME STEP
     #ans[0], ans[-1] = 1, 0
     ans[0] = ans[1]
     ans[-1] = ans[-2]
@@ -26,12 +27,12 @@ def step(x, y, t, dx, dt, f):
 
 zero = lambda *args: 0
 
-n, nt = 10**2, 10**9
+n, nt = 10**1, 10**9
 x = numpy.linspace(0, 1, n)
 y = (x < 0.5) * 1.0
 
 dx = 1/n
-dt = 0.5 * dx * dx
+dt = 0.3 * dx * dx
 
 lst = y
 t = 0
@@ -44,7 +45,8 @@ figure.canvas.flush_events()
 for i in range(nt):
     a, d = step(x, lst, i*dt, dx, dt, zero)
     lst = a
-    if time.time() - t > 0.1:
+    time.sleep(0.01)
+    if time.time() - t > 0.03:
         print(dt, i, i*dt)
         line1.set_ydata(lst)
         figure.canvas.draw()
