df2dde435a0df51138bb61a59966efd42c79c055
abrikos <abrikos1100@gmail.com>
2022-09-25 21:16:16 +0300

    Dynamic dt

--- a.py
+++ a.py
@@ -22,32 +22,57 @@ def step(x, y, t, dx, dt, f):
     ans[0] = ans[1]
     ans[-1] = ans[-2]
 
-    return ans, d2
+    return ans
+
+
+def nsteps(x, y, t, dx, dt, f, n):
+    ans = y.copy()
+    for i in range(n):
+        ans = step(x, ans, t + i*dt/n, dx, dt/n, f)
+    return ans
+def runge_step_time(x, y, t, dx, dt, f, n, maxn=1e4, eps=1e-3):
+    y1 = nsteps(x, y, t, dx, dt, f, n)
+    y2 = nsteps(x, y, t, dx, dt, f, n*2)
+    while (y1 - y2).max() < eps / 2 and n > 1:
+        y2 = y1
+        n = n // 2
+        y1 = nsteps(x, y, t, dx, dt, f, n)
+    while (y1 - y2).max() > eps and 2*n <= maxn:
+        y1 = y2
+        n *= 2
+        y2 = nsteps(x, y, t, dx, dt, f, n*2)
+
+    return n, y2
 
 
 zero = lambda *args: 0
 
-n, nt = 10**1, 10**9
+n, nt = 10**2, 10**9
 x = numpy.linspace(0, 1, n)
-y = (x < 0.5) * 1.0
+y = (x < 2/3) * (x > 1/3) * 1.0
 
 dx = 1/n
-dt = 0.3 * dx * dx
+dt = 0.5 * dx * dx
 
 lst = y
 t = 0
+n = 1
 
 plt.ion()
 figure, ax = plt.subplots(figsize=(10, 8))
+
 line1, = ax.plot(x, y)
+figure.canvas.flush_events()
 figure.canvas.draw()
 figure.canvas.flush_events()
+
+time.sleep(1)
 for i in range(nt):
-    a, d = step(x, lst, i*dt, dx, dt, zero)
+    n, a = runge_step_time(x, lst, i*dt, dx, dt, zero, n, eps=1e-6)
     lst = a
     time.sleep(0.01)
     if time.time() - t > 0.03:
-        print(dt, i, i*dt)
+        print(dt/n, n, i*dt)
         line1.set_ydata(lst)
         figure.canvas.draw()
         figure.canvas.flush_events()
